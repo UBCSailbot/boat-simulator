7bc41583d185717cd65c7b3b98f137c6cebe67ac
DFriend01 <devon.fri.2000@gmail.com>
2023-07-23 14:47:48 -0700

    Add utils file

---
+++ boat_simulator/common/utils.py
@@ -0,0 +1,13 @@
+from boat_simulator.common.types import Scalar
+
+
+def bound_to_180(angle: Scalar) -> Scalar:
+    raise NotImplementedError()
+
+
+def symmetric_clockwise_angle_to_true_bearing(angle: Scalar) -> Scalar:
+    raise NotImplementedError()
+
+
+def true_bearing_to_symmetric_clockwise_angle(angle: Scalar) -> Scalar:
+    raise NotImplementedError()
