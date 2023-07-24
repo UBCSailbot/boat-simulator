e2e2f6a1cf3797bd78eda34b89eee733facade5f
DFriend01 <devon.fri.2000@gmail.com>
2023-07-23 21:59:03 -0700

    Add documentation to control script

--- boat_simulator/nodes/low_level_control/control.py
+++ boat_simulator/nodes/low_level_control/control.py
@@ -1,19 +1,41 @@
+"""Low level control logic for actuating the rudder and the sail."""
+
 from boat_simulator.common.types import Scalar
-from typing import List
+from typing import Any, List
 from abc import ABC, abstractmethod
 
 
 class PID(ABC):
 
+    """Abstract class for a PID controller.
+
+    Attributes:
+        `kp` (Scalar): The proportional component tuning constant.
+        `ki` (Scalar): The integral component tuning constant.
+        `kd` (Scalar): The derivative component tuning constant.
+        `time_period` (Scalar): Constant time period between error samples.
+        `buf_size` (int): The max number of error samples to store for integral component.
+        `error_timeseries` (List[Scalar]): Timeseries of error values computed over time.
+    """
+
     # Private class member defaults
     __kp = 0
     __ki = 0
     __kd = 0
-    __buf_size = 50
     __time_period = 1
+    __buf_size = 50
     __error_timeseries = list()
 
     def __init__(self, kp: Scalar, ki: Scalar, kd: Scalar, time_period: Scalar, buf_size: int):
+        """Initializes the class attributes. Note that this class cannot be directly instantiated.
+
+        Args:
+            `kp` (Scalar): The proportional component tuning constant.
+            `ki` (Scalar): The integral component tuning constant.
+            `kd` (Scalar): The derivative component tuning constant.
+            `time_period` (Scalar): Time period between error samples.
+            `buf_size` (int): The max number of error samples to store for integral component.
+        """
         self.__kp = kp
         self.__ki = ki
         self.__kd = kd
@@ -21,26 +43,74 @@ class PID(ABC):
         self.__time_period = time_period
         self.__error_timeseries = list()
 
-    def step(self, current: Scalar, target: Scalar) -> Scalar:
+    def step(self, current: Any, target: Any) -> Scalar:
+        """Computes the correction factor.
+
+        Args:
+            `current` (Any): Current state of the system.
+            `target` (Any): Target state of the system.
+
+        Returns:
+            Scalar: Correction factor.
+        """
+        raise NotImplementedError()
+
+    def reset(self, is_latest_error_kept: bool = False):
+        """Empties the error timeseries of the PID controller, effectively starting a new
+        control iteration.
+
+        Args:
+            is_latest_error_kept (bool, optional): True if the latest error is kept in the error
+                timeseries to avoid starting from scratch if the target remains the same. False
+                if the timeseries should be completely emptied. Defaults to False.
+        """
         raise NotImplementedError()
 
     def __append_error(self, error: Scalar):
+        """Appends the latest error to the error timeseries attribute. If the timeseries is at
+        the maximum buffer size, the least recently computed error is evicted from the timeseries
+        and the new one is appended.
+
+        Args:
+            `error` (Scalar): The latest error.
+        """
         raise NotImplementedError()
 
     @abstractmethod
-    def _compute_error(self, current: Scalar, target: Scalar) -> Scalar:
+    def _compute_error(self, current: Any, target: Any) -> Scalar:
+        """Computes the currently observed error.
+
+        Args:
+            current (Any): Current state of the system.
+            target (Any): Target state of the system.
+
+        Returns:
+            Scalar: Current error between the current and target states.
+        """
         pass
 
     @abstractmethod
     def _compute_proportional_response(self) -> Scalar:
+        """
+        Returns:
+            Scalar: The proportional component of the correction factor.
+        """
         pass
 
     @abstractmethod
     def _compute_integral_response(self) -> Scalar:
+        """
+        Returns:
+            Scalar: The integral component of the correction factor.
+        """
         pass
 
     @abstractmethod
     def _compute_derivative_response(self) -> Scalar:
+        """
+        Returns:
+            Scalar: The derivative component of the correction factor.
+        """
         pass
 
     @property
@@ -69,8 +139,21 @@ class PID(ABC):
 
 
 class RudderPID(PID):
+    """Class for the rudder PID controller.
+
+    Extends: PID
+    """
 
     def __init__(self, kp: Scalar, ki: Scalar, kd: Scalar, time_period: Scalar, buf_size: int):
+        """Initializes the class attributes.
+
+        Args:
+            `kp` (Scalar): The proportional component tuning constant.
+            `ki` (Scalar): The integral component tuning constant.
+            `kd` (Scalar): The derivative component tuning constant.
+            `time_period` (Scalar): Time period between error samples.
+            `buf_size` (int): The max number of error samples to store for integral component.
+        """
         super().__init__(kp, ki, kd, time_period, buf_size)
 
     def _compute_error(self, current: Scalar, target: Scalar) -> Scalar:
