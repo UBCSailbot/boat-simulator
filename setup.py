75cdbbcb276e696667d5af1cf2c553673adc5a31
DFriend01 <devon.fri.2000@gmail.com>
2023-07-24 22:57:12 -0700

    Format with black

--- setup.py
+++ setup.py
@@ -3,11 +3,7 @@ from glob import glob
 from setuptools import setup, find_packages
 
 PACKAGE_NAME = 'boat_simulator'
-REQUIRED_MODULES = [
-    'setuptools',
-    'numpy',
-    'scipy'
-]
+REQUIRED_MODULES = ['setuptools', 'numpy', 'scipy']
 
 setup(
     name=PACKAGE_NAME,
@@ -15,18 +11,9 @@ setup(
     packages=find_packages(exclude=["tests"]),
     install_requires=REQUIRED_MODULES,
     data_files=[
-        (
-            'share/ament_index/resource_index/packages',
-            ['resource/' + PACKAGE_NAME]
-        ),
-        (
-            join('share', PACKAGE_NAME),
-            glob('launch/*launch.[pxy][yma]*')
-        ),
-        (
-            join('share', PACKAGE_NAME),
-            ['package.xml']
-        ),
+        ('share/ament_index/resource_index/packages', ['resource/' + PACKAGE_NAME]),
+        (join('share', PACKAGE_NAME), glob('launch/*launch.[pxy][yma]*')),
+        (join('share', PACKAGE_NAME), ['package.xml']),
     ],
     zip_safe=True,
     maintainer='Devon Friend',
@@ -36,8 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'physics_engine_node = ' +
-            'boat_simulator.nodes.physics_engine.physics_engine_node:main',
+            'physics_engine_node = boat_simulator.nodes.physics_engine.physics_engine_node:main',
         ],
     },
 )
