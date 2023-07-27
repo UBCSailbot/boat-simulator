cacd6d4050895a597528bf8a549fa22bde8dd400
DFriend01 <devon.fri.2000@gmail.com>
2023-07-27 00:25:54 -0700

    Add test file for generators

---
+++ tests/unit/common/test_generators.py
@@ -0,0 +1,13 @@
+"""Tests classes and functions in boat_simulator/common/generators.py"""
+
+
+class TestGaussianGenerator:
+    pass
+
+
+class TestMVGaussianGenerator:
+    pass
+
+
+class TestConstantGenerator:
+    pass
