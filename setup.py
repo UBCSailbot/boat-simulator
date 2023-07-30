783a430b20cf8e3ab2102b0b901a9f495cb44650
Patrick Creighton <pcreighton429@gmail.com>
2023-07-29 18:14:20 -0700

    Format and fix setup.py

--- setup.py
+++ setup.py
@@ -1,43 +1,31 @@
-from os.path import join
 from glob import glob
-from setuptools import setup, find_packages
+from os.path import join
 
-PACKAGE_NAME = 'boat_simulator'
-REQUIRED_MODULES = [
-    'setuptools',
-    'numpy',
-    'scipy'
-]
+from setuptools import find_packages, setup
+
+PACKAGE_NAME = "boat_simulator"
+REQUIRED_MODULES = ["setuptools", "numpy", "scipy"]
 
 setup(
     name=PACKAGE_NAME,
-    version='0.0.0',
+    version="0.0.0",
     packages=find_packages(exclude=["tests"]),
     install_requires=REQUIRED_MODULES,
     data_files=[
-        (
-            'share/ament_index/resource_index/packages',
-            ['resource/' + PACKAGE_NAME]
-        ),
-        (
-            join('share', PACKAGE_NAME, 'launch'),
-            glob('launch/*launch.[pxy][yma]*')
-        ),
-        (
-            join('share', PACKAGE_NAME),
-            ['package.xml']
-        ),
+        ("share/ament_index/resource_index/packages", ["resource/" + PACKAGE_NAME]),
+        ("share/" + PACKAGE_NAME, ["package.xml"]),
+        (join("share", PACKAGE_NAME), glob("launch/*_launch.py")),
     ],
     zip_safe=True,
-    maintainer='Devon Friend',
-    maintainer_email='software@ubcsailbot.org',
-    description='UBC Sailbot\'s Boat Simulator',
-    license='Apache License 2.0',
-    tests_require=['pytest'],
+    maintainer="Devon Friend",
+    maintainer_email="software@ubcsailbot.org",
+    description="UBC Sailbot's Boat Simulator",
+    license="Apache License 2.0",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'physics_engine_node = ' +
-            'boat_simulator.nodes.physics_engine.physics_engine_node:main',
+        "console_scripts": [
+            "physics_engine_node = "
+            + "boat_simulator.nodes.physics_engine.physics_engine_node:main",
         ],
     },
 )
