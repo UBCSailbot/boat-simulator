10624cd11fb39dc1aca2d5f8a6189a1f12c0b5b9
DFriend01 <devon.fri.2000@gmail.com>
2023-08-03 00:34:40 -0700

    Reformat physics engine node

--- boat_simulator/nodes/physics_engine/physics_engine_node.py
+++ boat_simulator/nodes/physics_engine/physics_engine_node.py
@@ -7,14 +7,14 @@ from std_msgs.msg import String
 
 class PhysicsEngineNode(Node):
     def __init__(self):
-        super().__init__('my_node')
-        self.publisher_ = self.create_publisher(String, 'physics_engine_out', 10)
+        super().__init__("my_node")
+        self.publisher_ = self.create_publisher(String, "physics_engine_out", 10)
         self.timer_ = self.create_timer(1.0, self.timer_callback)
         self.counter_ = 0
 
     def timer_callback(self):
         msg = String()
-        msg.data = 'Hello World! %d' % self.counter_
+        msg.data = "Hello World! %d" % self.counter_
         self.publisher_.publish(msg)
         self.counter_ += 1
 
@@ -26,5 +26,5 @@ def main(args=None):
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
