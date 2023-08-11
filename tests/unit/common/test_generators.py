ca397713851a3f4befc4079a671388e5561a3219
Steven Xu <steven.x2085@gmail.com>
2023-08-10 20:24:03 -0700

    multivariate vector generation test

--- tests/unit/common/test_generators.py
+++ tests/unit/common/test_generators.py
@@ -1,12 +1,20 @@
 """Tests classes and functions in boat_simulator/common/generators.py"""
 
+import numpy as np
+import pytest
+
 
 class TestGaussianGenerator:
     pass
 
 
 class TestMVGaussianGenerator:
-    pass
+    @pytest.mark.parametrize(
+        "mean, cov, num_samples",
+        [(60, 1, 60), (0.5, 2, 0.5 * 2), (-10.2, 14.22, -10.2 * 14.22)],
+    )
+    def test_multivariate_vector_generation(self, mean, cov, num_samples):
+        pass
 
 
 class TestConstantGenerator:
