5324564c2fc136c9126a4863e936c4ac3f46f23e
eomielan <eomielan@gmail.com>
2023-09-10 15:35:12 -0700

    Add Field[] to type hints, add TODO for diff between ref and global frames

--- boat_simulator/nodes/physics_engine/kinematics_data.py
+++ boat_simulator/nodes/physics_engine/kinematics_data.py
@@ -1,15 +1,16 @@
 """This module contains the kinematics data for the boat."""
 
-from dataclasses import dataclass, field
+from dataclasses import Field, dataclass, field
 from numpy.typing import ArrayLike
 import numpy as np
 
 
 @dataclass
 class KinematicsData:
-    linear_position: ArrayLike = field(default=np.zeros(3, dtype=np.float32))
-    linear_velocity: ArrayLike = field(default=np.zeros(3, dtype=np.float32))
-    linear_acceleration: ArrayLike = field(default=np.zeros(3, dtype=np.float32))
-    angular_position: ArrayLike = field(default=np.zeros(3, dtype=np.float32))
-    angular_velocity: ArrayLike = field(default=np.zeros(3, dtype=np.float32))
-    angular_acceleration: ArrayLike = field(default=np.zeros(3, dtype=np.float32))
+    # TODO: Ensure position is always set to 0 for relative reference frame
+    linear_position: Field[ArrayLike] = field(default=np.zeros(3, dtype=np.float32))
+    linear_velocity: Field[ArrayLike] = field(default=np.zeros(3, dtype=np.float32))
+    linear_acceleration: Field[ArrayLike] = field(default=np.zeros(3, dtype=np.float32))
+    angular_position: Field[ArrayLike] = field(default=np.zeros(3, dtype=np.float32))
+    angular_velocity: Field[ArrayLike] = field(default=np.zeros(3, dtype=np.float32))
+    angular_acceleration: Field[ArrayLike] = field(default=np.zeros(3, dtype=np.float32))
