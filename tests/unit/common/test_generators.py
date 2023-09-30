5388f1fff44f8c725a61ac0e135e39cbe8edec04
Steven Xu <steven.x2085@gmail.com>
2023-09-30 15:14:52 -0700

    MVG Testing

--- tests/unit/common/test_generators.py
+++ tests/unit/common/test_generators.py
@@ -1,8 +1,10 @@
 """Tests classes and functions in boat_simulator/common/generators.py"""
-from boat_simulator.common.generators import MVGaussianGenerator
+import math
+
 import numpy as np
 import pytest
-import math
+
+from boat_simulator.common.generators import MVGaussianGenerator
 
 
 class TestGaussianGenerator:
@@ -11,19 +13,32 @@ class TestGaussianGenerator:
 
 class TestMVGaussianGenerator:
     @pytest.mark.parametrize(
-        "mean, cov, num_samples",
-        [(60, 1, 60), (0.5, 2, 0.5 * 2), (-10.2, 14.22, -10.2 * 14.22)],
+        "mean, cov",
+        [
+            (np.array([1, 1]), np.eye(2)),
+            (np.array([1, 1, 1]), np.eye(3)),
+            (np.array([1, 1, 1, 1]), np.eye(4)),
+        ],
     )
-    def test_multivariate_vector_generation(self, mean, cov, num_samples): # did not use cov
-        samples = list()
-        vector_total = 0
-        for i in range(1, num_samples): 
-            sample = MVGaussianGenerator() # will this generate random vectors?
-            vector_total += sample
-            samples.insert(i, sample)
-        
-        actual_mean = vector_total / i
-        assert math.isclose(actual_mean, mean)
+    def test_multivariate_vector_generation_2d(self, mean, cov):
+        """This test compares the calculated cov/mean of generated vectors against
+        expected mean/cov arrays
+
+        Args:
+            mean (array): Input array of size n with average 1
+            cov (array): Identity matrix based on mean array size (I_n)
+        """
+        NUM_SAMPLES = 5000
+        samples = np.zeros(shape=(NUM_SAMPLES, mean.size))
+        generator = MVGaussianGenerator(mean=mean, cov=cov)
+        for i in range(NUM_SAMPLES):
+            samples[i, :] = generator.next()
+        sample_mean = np.sum(samples, axis=0) / NUM_SAMPLES
+        sample_cov = np.cov(samples, rowvar=False)
+
+        assert np.allclose(sample_cov, cov, atol=0.1)
+        assert np.isclose(sample_mean, mean, 0.1).all()
+
 
 class TestConstantGenerator:
     pass
