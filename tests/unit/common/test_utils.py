6d24c27d3dc08bde59f8c9ef9cbf16c61d5684cd
eomielan <eomielan@gmail.com>
2023-09-30 18:49:48 -0700

    Add unit tests for bound_to_180

--- tests/unit/common/test_utils.py
+++ tests/unit/common/test_utils.py
@@ -1,8 +1,10 @@
 """Test the functions in boat_simulator/common/utils.py"""
 
-from boat_simulator.common import utils
-import pytest
 import math
+
+import pytest
+
+from boat_simulator.common import utils
 
 
 @pytest.mark.parametrize(
@@ -40,11 +42,11 @@ def test_degrees_to_rad(test_input, expected_output):
 @pytest.mark.parametrize(
     "test_input1, test_input2, expected_output",
     [
-        ([0, 2*math.pi], True, [0, 0]),
-        ([3/2*math.pi, -2.5*math.pi], True, [-0.5*math.pi, -0.5*math.pi]),
-        ([3*math.pi, -3*math.pi], True, [-math.pi, -math.pi]),
-        ([4.44*math.pi, -5.68*math.pi], True, [0.44*math.pi, 0.32*math.pi]),
-        ([1/36*math.pi, -0.334*math.pi], True, [1/36*math.pi, -0.334*math.pi]),
+        ([0, 2 * math.pi], True, [0, 0]),
+        ([3 / 2 * math.pi, -2.5 * math.pi], True, [-0.5 * math.pi, -0.5 * math.pi]),
+        ([3 * math.pi, -3 * math.pi], True, [-math.pi, -math.pi]),
+        ([4.44 * math.pi, -5.68 * math.pi], True, [0.44 * math.pi, 0.32 * math.pi]),
+        ([1 / 36 * math.pi, -0.334 * math.pi], True, [1 / 36 * math.pi, -0.334 * math.pi]),
         ([0, 270, -450, 360], False, [0, -90, -90, 0]),
         ([540, -540, 899, -899, 5, -30], False, [-180, -180, 179, -179, 5, -30]),
     ],
