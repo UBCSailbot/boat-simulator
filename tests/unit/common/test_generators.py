6a0a863c4676c3cc5aa9715cb5b223de58c8b8ef
Steven Xu <steven.x2085@gmail.com>
2023-10-07 10:19:21 -0700

    Additional Test Cases Commit

--- tests/unit/common/test_generators.py
+++ tests/unit/common/test_generators.py
@@ -16,8 +16,13 @@ class TestMVGaussianGenerator:
         "mean, cov",
         [
             (np.array([1, 1]), np.eye(2)),
+            (np.array([1, 2]), np.array([[2, 1], [1, 2]])),
+            (np.array([4, 5]), np.array([[3, 1], [1, 3]])),
+            (np.array([100, 50]), np.array([[10, 5], [5, 10]])),
             (np.array([1, 1, 1]), np.eye(3)),
-            (np.array([1, 1, 1, 1]), np.eye(4)),
+            (np.array([1, 2, 3]), np.array([[2, 1, 1], [1, 2, 1], [1, 1, 2]])),
+            (np.array([4, 5, 6]), np.array([[3, 2, 2], [2, 3, 2], [2, 2, 3]])),
+            (np.array([120, 130, 140]), np.array([[20, 10, 10], [10, 20, 10], [10, 10, 20]])),
         ],
     )
     def test_multivariate_vector_generation_2d(self, mean, cov):
@@ -28,15 +33,15 @@ class TestMVGaussianGenerator:
             mean (array): Input array of size n with average 1
             cov (array): Identity matrix based on mean array size (I_n)
         """
-        NUM_SAMPLES = 5000
+        NUM_SAMPLES = 20000
         samples = np.zeros(shape=(NUM_SAMPLES, mean.size))
         generator = MVGaussianGenerator(mean=mean, cov=cov)
         for i in range(NUM_SAMPLES):
             samples[i, :] = generator.next()
-        sample_mean = np.sum(samples, axis=0) / NUM_SAMPLES
+        sample_mean = np.mean(samples, axis=0)
         sample_cov = np.cov(samples, rowvar=False)
 
-        assert np.allclose(sample_cov, cov, atol=0.1)
+        assert np.allclose(sample_cov, cov, atol=0.2)
         assert np.isclose(sample_mean, mean, 0.1).all()
 
 
