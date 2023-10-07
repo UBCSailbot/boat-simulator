d29260cadc27421a0304676eaccefcfca11d9aad
Steven Xu <125757261+stevenxu27@users.noreply.github.com>
2023-10-07 16:16:55 -0700

    Implement multivariate gaussian vector generator (#36)
    * Implement the start of the number generator class
    
    * Add template classes and documentation
    
    * Add test file for generators
    
    * Add seeded number generator
    
    * multivariate vector generation test
    
    * Initial test case for MVG Generator
    
    * MVG Testing
    
    * Additional Test Cases Commit
    
    * Style Fixes
    
    * Test Case Fix
    
    * Test cases
    
    * Change typing and update documentation in generators.py
    
    * Add ScalarOrArray type
    
    * Change precision of ints and floats in ScalarOrArray type
    
    ---------
    
    Co-authored-by: DFriend01 <devon.fri.2000@gmail.com>

---
+++ tests/unit/common/test_generators.py
@@ -0,0 +1,48 @@
+"""Tests classes and functions in boat_simulator/common/generators.py"""
+
+import numpy as np
+import pytest
+
+from boat_simulator.common.generators import MVGaussianGenerator
+
+
+class TestGaussianGenerator:
+    pass
+
+
+class TestMVGaussianGenerator:
+    @pytest.mark.parametrize(
+        "mean, cov",
+        [
+            (np.array([1, 1]), np.eye(2)),
+            (np.array([1, 2]), np.array([[2, 1], [1, 2]])),
+            (np.array([4, 5]), np.array([[3, 1], [1, 3]])),
+            (np.array([100, 50]), np.array([[10, 5], [5, 10]])),
+            (np.array([120, 130]), np.array([[10, 5], [5, 10]])),
+            (np.array([1, 1, 1]), np.eye(3)),
+            (np.array([1, 2, 3]), np.array([[2, 1, 1], [1, 2, 1], [1, 1, 2]])),
+            (np.array([4, 5, 6]), np.array([[3, 2, 2], [2, 3, 2], [2, 2, 3]])),
+        ],
+    )
+    def test_multivariate_vector_generation_2d(self, mean, cov):
+        """This test compares the calculated cov/mean of generated vectors against
+        expected mean/cov arrays
+
+        Args:
+            mean (array): Input array of size n with average 1
+            cov (array): Identity matrix based on mean array size (I_n)
+        """
+        NUM_SAMPLES = 10000
+        samples = np.zeros(shape=(NUM_SAMPLES, mean.size))
+        generator = MVGaussianGenerator(mean=mean, cov=cov)
+        for i in range(NUM_SAMPLES):
+            samples[i, :] = generator.next()
+        sample_mean = np.mean(samples, axis=0)
+        sample_cov = np.cov(samples, rowvar=False)
+
+        assert np.allclose(sample_cov, cov, atol=0.2)
+        assert np.isclose(sample_mean, mean, 0.1).all()
+
+
+class TestConstantGenerator:
+    pass
