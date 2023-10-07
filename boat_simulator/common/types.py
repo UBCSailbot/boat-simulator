b80d2899ec53f895e0e6dccaa6009f866b9f3fae
Chris Chang <64388914+chrischang5@users.noreply.github.com>
2023-10-07 16:00:56 -0700

    Implement UnitConverter class (#35)
    * ConversionFactors: Add conversion constants
    
    * UnitConverter: __init__ and convert functions
    
    * UnitConvertor: Add nautical mile unit conversions
    
    * UnitConvertor: Add length unit conversion tests
    
    * UnitConvertor: time unit conversion tests
    
    * UnitConvertor: Add mass and acceleration units/tests
    
    * UnitConverter: Update test values
    
    * UnitConverter: angle conversion factors and parameterized tests
    
    * UnitConverter: radians to degrees test
    
    * UnitConverter: Extend unit conversions to np arrays
    
    * UnitConverter: Reinstate Scalar type where appropriate
    
    * UnitConverter: simplify ArrayLike import
    
    * UnitConverter: modify typing of convert functions to take Scalars or NDArrays
    
    * Types: Create ScalarOrArray type

--- boat_simulator/common/types.py
+++ boat_simulator/common/types.py
@@ -1,11 +1,16 @@
 """Custom types used for type hinting in the boat simulator package."""
 
 from enum import Enum
-from typing import Union, TypeVar
+from typing import TypeVar, Union
 
+import numpy as np
+from numpy.typing import NDArray
 
 # Any attribute of a class that extends Enum
-EnumAttr = TypeVar('EnumAttr', bound=Enum)
+EnumAttr = TypeVar("EnumAttr", bound=Enum)
 
 # Scalar value that can be an integer or a float
 Scalar = Union[int, float]
+
+# Used in cases where support for scalars or arrays of scalars are needed.
+ScalarOrArray = Union[Scalar, NDArray[np.int], NDArray[np.float]]
