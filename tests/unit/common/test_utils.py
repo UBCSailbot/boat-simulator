39b1f554870df037cbf75a3a7bf9bd14dcf03d44
Steven Xu <steven.x2085@gmail.com>
2023-10-21 15:47:22 -0700

    Angle conversion and heading conversion deletion

--- tests/unit/common/test_utils.py
+++ tests/unit/common/test_utils.py
@@ -74,3 +74,39 @@ def test_degrees_to_rad(test_input, expected_output):
 def test_bound_to_180(angle, isDegrees, expected_output):
     actual_output = utils.bound_to_180(angle, isDegrees)
     assert np.isclose(actual_output, expected_output).all()
+
+
+@pytest.mark.parametrize(
+    "angle, isDegrees, expected_output",
+    [
+        # Degree tests
+        (0, True, 0),
+        (360, True, 0),
+        (-270, True, 90),
+        (750.2, True, 30.2),
+        (
+            np.array([754.4, -540, 899, 719.99, 5, -30.74]),
+            True,
+            np.array([34.4, 180, 179, 359.99, 5, 329.26]),
+        ),
+        # Radian tests
+        (0, False, 0),
+        (2 * math.pi, False, 0),
+        (1.5 * math.pi, False, 1.5 * math.pi),
+        (-2.5 * math.pi, False, 1.5 * math.pi),
+        (np.array([3 * math.pi, -3 * math.pi]), False, np.array([math.pi, math.pi])),
+        (
+            np.array([4.44 * math.pi, -5.68 * math.pi]),
+            False,
+            np.array([0.44 * math.pi, 0.32 * math.pi]),
+        ),
+        (
+            np.array([1 / 36 * math.pi, -0.334 * math.pi]),
+            False,
+            np.array([1 / 36 * math.pi, 1.666 * math.pi]),
+        ),
+    ],
+)
+def test_bound_to_360(angle, isDegrees, expected_output):
+    actual_output = utils.bound_to_360(angle, isDegrees)
+    assert np.isclose(actual_output, expected_output).all()
