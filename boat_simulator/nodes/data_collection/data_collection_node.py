69fd0fe5631ff5784900348964e4e10438068dbd
eomielan <eomielan@gmail.com>
2023-11-03 23:41:38 -0700

    Update init subscriptions and prepare dict of msg types

--- boat_simulator/nodes/data_collection/data_collection_node.py
+++ boat_simulator/nodes/data_collection/data_collection_node.py
@@ -4,13 +4,14 @@
 
 import inspect
 import sys
+from typing import Type
 
-import custom_interfaces.msg as msg_types
+import custom_interfaces.msg
 import rclpy
 import rclpy.utilities
 import rosbag2_py
-from rclpy.executors import SingleThreadedExecutor
 from rclpy.node import Node
+from rclpy.serialization import serialize_message
 
 import boat_simulator.common.constants as Constants
 
@@ -18,9 +19,7 @@ import boat_simulator.common.constants as Constants
 def main(args=None):
     rclpy.init(args=args)
     node = DataCollectionNode()
-    executor = SingleThreadedExecutor()
-    executor.add_node(node)
-    executor.spin()
+    rclpy.spin(node)
     rclpy.shutdown()
 
 
@@ -67,8 +66,9 @@ class DataCollectionNode(Node):
     def __init_msg_types_dict(self) -> None:
         """Prepare dictionary of all msg types with key name and value class"""
         self.__msg_types_dict = {}
-        for name, cls in inspect.getmembers(msg_types, inspect.isclass):
-            self.__msg_types_dict[name] = cls
+        for name, cls in inspect.getmembers(custom_interfaces.msg, inspect.isclass):
+            if not name.startswith("_"):
+                self.__msg_types_dict[name] = cls
 
     def __init_private_attributes(self):
         if self.get_parameter("bag").get_parameter_value().bool_value:
@@ -78,30 +78,56 @@ class DataCollectionNode(Node):
             )
             converter_options = rosbag2_py._storage.ConverterOptions("", "")
             self.__writer.open(storage_options, converter_options)
+
         if self.get_parameter("json").get_parameter_value().bool_value:
             # add json init
             pass
 
-    def __init_callback_groups(self):
-        """Initializes the callback groups."""
-        self.get_logger().debug("Initializing callback groups...")
-        # add here when implemented
-
     def __init_subscriptions(self):
         self.get_logger().debug("Initializing subscriptions...")
-        classe_names = [name for name, cls in inspect.getmembers(msg_types, inspect.isclass)]
-        for i in range(len(self.get_parameter("topics").get_parameter_value().string_array_value)):
-            pass
-        self.__desired_heading_sub = self.create_subscription(
-            msg_type=DesiredHeading,
-            topic=Constants.PHYSICS_ENGINE_SUBSCRIPTIONS.DESIRED_HEADING,
-            callback=self.__desired_heading_sub_callback,
-            qos_profile=self.get_parameter("qos_depth").get_parameter_value().integer_value,
-            callback_group=self.sub_callback_group,
+
+        topics_to_record = self.get_parameter("topics").get_parameter_value().string_array_value
+
+        # assuming topics are specified as ['topic_name', 'topic_type']
+        for i in range(1, len(topics_to_record), 2):
+            if topics_to_record[i] in self.__msg_types_dict:
+                self.__record_topics_sub.append(
+                    self.create_subscription(
+                        msg_type=self.__msg_types_dict[topics_to_record[i]],
+                        # what happens when try to record topic not running?
+                        topic=topics_to_record[i - 1],
+                        callback=self.__general_sub_callback,  # to change when implemented
+                        qos_profile=self.get_parameter("qos_depth")
+                        .get_parameter_value()
+                        .integer_value,
+                    )
+                )
+            else:
+                # Display Error
+                pass
+
+            if self.get_parameter("bag").get_parameter_value().bool_value:
+                topic_info = rosbag2_py._storage.TopicMetadata(
+                    name=topics_to_record[i - 1], type="", serialization_format="cdr"
+                )
+                self.__writer.create_topic(topic_info)
+
+    # SUBSCRIPTION CALLBACKS
+    def __general_sub_callback(self, msg: Type):
+        if self.get_parameter("bag").get_parameter_value().bool_value:
+            self.__bag_sub_callback(msg)
+        if self.get_parameter("json").get_parameter_value().bool_value:
+            self.__json_sub_callback(msg)
+
+    # Use closures: https://www.geeksforgeeks.org/python-closures/
+    # and higher order functions: https://geeksforgeeks.org/higher-order-functions-in-python/
+    # Print warning maybe for nodes that haven't published in a while?
+    # alternative to checking if node specified in globals.yaml is actually launched
+
+    def __bag_sub_callback(self, msg: Type):
+        self.__writer.write(
+            "topic_name", serialize_message(msg), self.get_clock().now().nanoseconds
         )
-        topics_to_record = []
-        for topic in topics_to_record:
-            topic_info = rosbag2_py._storage.TopicMetadata(
-                name=topic, type="", serialization_format="cdr"
-            )
-            self.writer.create_topic(topic_info)
+
+    def __json_sub_callback(self, msg: Type):
+        pass
