a07a5849b1808ef69ac268795de8ec15d22585df
eomielan <eomielan@gmail.com>
2023-11-04 17:08:51 -0700

    Update sub and timer callbacks form bag and json

--- boat_simulator/nodes/data_collection/data_collection_node.py
+++ boat_simulator/nodes/data_collection/data_collection_node.py
@@ -3,6 +3,7 @@
 """The ROS node for data collection."""
 
 import inspect
+import json
 import sys
 from typing import Type
 
@@ -10,6 +11,7 @@ import custom_interfaces.msg
 import rclpy
 import rclpy.utilities
 import rosbag2_py
+import rosidl_runtime_py
 from rclpy.node import Node
 from rclpy.serialization import serialize_message
 
@@ -39,15 +41,17 @@ class DataCollectionNode(Node):
         super.__init__("data_collection_node")
         self.get_logger().debug("Initializing node...")
         self.__declare_ros_parameters()
-        self.__init_private_attributes()
-        # add rest of init helpers
+        self.__init_config_attributes()
+        self.__init_storage_files()
+        self.__init_msg_types_dict()
+        self.__init_subscriptions()
+        self.__init_timer_callbacks()
         self.get_logger().debug("Node initialization complete. Starting execution...")
 
     def __declare_ros_parameters(self):
         """Declares ROS parameters from the global configuration file that will be used in this
         node.
         """
-        # assume topis will be an array where it alternates topic then topic type
         self.get_logger().debug("Declaring ROS parameters...")
         self.declare_parameters(
             namespace="",
@@ -63,71 +67,103 @@ class DataCollectionNode(Node):
             value_str = str(parameter.value)
             self.get_logger().debug(f"Got parameter {name} with value {value_str}")
 
-    def __init_msg_types_dict(self) -> None:
+    def __init_config_attributes(self):
+        """Initialize ROS parameters from the global configuration as private attributes."""
+        self.__file_name = self.get_parameter("file_name").get_parameter_value().string_value
+        self.__record_topics = (
+            self.get_parameter("topics").get_parameter_value().string_array_value
+        )
+        self.__use_bag = self.get_parameter("bag").get_parameter_value().bool_value
+        self.__use_json = self.get_parameter("json").get_parameter_value().bool_value
+
+    def __init_storage_files(self):
+        if self.__use_bag:
+            self.__writer = rosbag2_py.SequentialWriter()
+            storage_options = rosbag2_py._storage.StorageOptions(
+                uri=self.__file_name, storage_id="sqlite3"
+            )
+            converter_options = rosbag2_py._storage.ConverterOptions("", "")
+            self.__writer.open(storage_options, converter_options)
+
+        if self.__use_json:
+            self.__data_to_write = {}
+            self.__json_counter = 0
+            json_file_path = "./" + self.__file_name + ".json"
+            self.__json_file = open(json_file_path, "a")
+            self.__json_file.write("[\n")
+
+    def __init_msg_types_dict(self):
         """Prepare dictionary of all msg types with key name and value class"""
         self.__msg_types_dict = {}
         for name, cls in inspect.getmembers(custom_interfaces.msg, inspect.isclass):
             if not name.startswith("_"):
                 self.__msg_types_dict[name] = cls
 
-    def __init_private_attributes(self):
-        if self.get_parameter("bag").get_parameter_value().bool_value:
-            self.__writer = rosbag2_py.SequentialWriter()
-            storage_options = rosbag2_py._storage.StorageOptions(
-                uri="data_collection_bag", storage_id="sqlite3"
-            )
-            converter_options = rosbag2_py._storage.ConverterOptions("", "")
-            self.__writer.open(storage_options, converter_options)
-
-        if self.get_parameter("json").get_parameter_value().bool_value:
-            # add json init
-            pass
-
     def __init_subscriptions(self):
+        """Initialize the subscriptions for this node. These subscriptions pertain to the topics
+        from which data will be collected."""
         self.get_logger().debug("Initializing subscriptions...")
 
-        topics_to_record = self.get_parameter("topics").get_parameter_value().string_array_value
+        def create_subscription_with_callback(topic_name, msg_type):
+            return self.create_subscription(
+                msg_type=msg_type,
+                topic=topic_name,
+                callback=lambda msg: self.__general_sub_callback(msg, topic_name),
+                qos_profile=self.get_parameter("qos_depth").get_parameter_value().integer_value,
+            )
 
         # assuming topics are specified as ['topic_name', 'topic_type']
-        for i in range(1, len(topics_to_record), 2):
-            if topics_to_record[i] in self.__msg_types_dict:
-                self.__record_topics_sub.append(
-                    self.create_subscription(
-                        msg_type=self.__msg_types_dict[topics_to_record[i]],
-                        # what happens when try to record topic not running?
-                        topic=topics_to_record[i - 1],
-                        callback=self.__general_sub_callback,  # to change when implemented
-                        qos_profile=self.get_parameter("qos_depth")
-                        .get_parameter_value()
-                        .integer_value,
-                    )
-                )
-            else:
-                # Display Error
-                pass
+        for i in range(1, len(self.__record_topics), 2):
+            topic_name = self.__record_topics[i - 1]
+            msg_type_name = self.__record_topics[i]
 
-            if self.get_parameter("bag").get_parameter_value().bool_value:
+            if msg_type_name not in self.__msg_types_dict:
+                # Display Error
+                continue
+
+            self.__record_topics_sub.append(
+                create_subscription_with_callback(topic_name, self.__msg_types_dict[msg_type_name])
+            )
+
+            if self.__use_bag:
                 topic_info = rosbag2_py._storage.TopicMetadata(
-                    name=topics_to_record[i - 1], type="", serialization_format="cdr"
+                    name=topic_name, type=msg_type_name, serialization_format="cdr"
                 )
                 self.__writer.create_topic(topic_info)
 
+            if self.__use_json:
+                self.__data_to_write[topic_name] = None
+
+    def __init_timer_callbacks(self):
+        """Initializes timer callbacks of this node that are executed periodically."""
+        self.get_logger().debug("Initializing timer callbacks...")
+
+        # specify timer period when known
+        self.create_timer(timer_period_sec="", callback=self.__write_to_json)
+
     # SUBSCRIPTION CALLBACKS
-    def __general_sub_callback(self, msg: Type):
-        if self.get_parameter("bag").get_parameter_value().bool_value:
-            self.__bag_sub_callback(msg)
-        if self.get_parameter("json").get_parameter_value().bool_value:
-            self.__json_sub_callback(msg)
+    def __general_sub_callback(self, msg: Type, topic_name: str):
+        if self.__use_bag:
+            self.__writer.write(
+                topic_name, serialize_message(msg), self.get_clock().now().nanoseconds
+            )
+        if self.__use_json:
+            msg_as_json = json.dumps(rosidl_runtime_py.message_to_ordereddict(msg), indent=4)
+            self.__data_to_write[topic_name] = msg_as_json
+
+    # TIMER CALLBACKS
+    def __write_to_json(self):
+        # Only concern with this is if topic subscribed to is not launched, then we will never
+        # write to json for all the other ones will never get written
+        if all(self.__data_to_write.values()):  # all values are not None
+            self.__data_to_write["time"] = self.get_clock().now().nanoseconds
+            self.__json_counter.write(",\n")  # don't print comma for last
+            item_to_write = {self.__json_counter: self.__data_to_write}
+            json.dump(item_to_write, self.__json_file, indent=4)
+            self.__json_counter += 1
 
     # Use closures: https://www.geeksforgeeks.org/python-closures/
     # and higher order functions: https://geeksforgeeks.org/higher-order-functions-in-python/
     # Print warning maybe for nodes that haven't published in a while?
     # alternative to checking if node specified in globals.yaml is actually launched
-
-    def __bag_sub_callback(self, msg: Type):
-        self.__writer.write(
-            "topic_name", serialize_message(msg), self.get_clock().now().nanoseconds
-        )
-
-    def __json_sub_callback(self, msg: Type):
-        pass
+    # Look at the rclpy.Context class for on_shutdown function. Define a shutdown callback
