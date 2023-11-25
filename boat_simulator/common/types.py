96c940564418a6e0b3eb38b482a0cdd24fa07062
Chris Chang <1chang1chang1@gmail.com>
2023-11-25 14:57:47 -0800

    Try to make CI happy

--- boat_simulator/common/types.py
+++ boat_simulator/common/types.py
@@ -1,11 +1,10 @@
 """Custom types used for type hinting in the boat simulator package."""
 
 from enum import Enum
-from typing import TypeVar, Union,
+from typing import TypeVar, Union
 
 import numpy as np
 from numpy.typing import NDArray
-
 
 
 # Any attribute of a class that extends Enum
@@ -16,4 +15,3 @@ Scalar = Union[int, float]
 
 # Used in cases where support for scalars or arrays of scalars are needed.
 ScalarOrArray = Union[Scalar, NDArray[Union[np.int32, np.float32]]]
-
